ddbc5a4e9d4662fe73e973d44f2c643c5f96bd3f
amangoyal018 <goyalamanmay18@gmail.com>
2023-09-16 23:00:36 +0530

    JS Part Done

--- script.js
+++ script.js
@@ -4,10 +4,34 @@ const squares = document.querySelector("#squares").children;
 for(let element of squares){
     element.classList.add("untapped");
 }
+let arr = [];
 for(let element of squares){
+
     element.addEventListener("click",(event) =>{
+
         element.classList.remove("untapped");
         element.classList.add("tapped");
 
+        let classname = element.getAttribute("class");
+        let num = classname[7];
+        arr.push(num);
+        // console.log(num);
+        if(num==9){
+            // console.log(...arr);
+            let index = 0;
+            function traverse(){
+                if(index<arr.length){
+                    squares[arr[index]-1].classList.remove("tapped"); 
+                    squares[arr[index]-1].classList.add("untapped");
+                    
+                    index++;
+                    setTimeout(() => traverse(arr), 400);
+                }else{
+                    arr = [];
+                    return;
+                }
+            }
+            traverse();
+        }
     })
 }
